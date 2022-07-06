4d933ab94013d1866b9b0891b4643c71c7f08370
S-B-A <sasha.berg.agency@gmail.com>
2022-07-06 20:39:01 +0300

    Тестовый запуск на смартфоне - успешно.

---
+++ app/src/main/java/ru/sashaberg/paramedic/BabyMenu.java
@@ -0,0 +1,14 @@
+package ru.sashaberg.paramedic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class BabyMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baby_menu);
+    }
+}
--- app/src/main/java/ru/sashaberg/paramedic/MainActivity.java
+++ app/src/main/java/ru/sashaberg/paramedic/MainActivity.java
@@ -1,7 +1,11 @@
 package ru.sashaberg.paramedic;
 
 import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickBaby(View view) {
+        Intent intent = new Intent(this, BabyMenu.class);
+        startActivity(intent);
+    }
+    public void onClickKids(View view) {
+        Intent intent = new Intent(this, KidsMenu.class);
+        startActivity(intent);
+    }
+    public void onClickTeens(View view) {
+        Intent intent = new Intent(this, TeensMenu.class);
+        startActivity(intent);
+    }
+
 }
